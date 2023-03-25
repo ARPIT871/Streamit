ef825e2441c9f0e532e4a75e7e97e1b0924c24f9
Arpit Rajput <87810588+ARPIT871@users.noreply.github.com>
2023-03-25 16:36:26 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': "f9d1ccb96amshfe97ad994947ac4p1b3907jsnb9597d71bd81",
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
