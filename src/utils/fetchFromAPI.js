720e62d2ff7c26e92890638396f03525fba07b7d
Arpit Rajput <87810588+ARPIT871@users.noreply.github.com>
2023-06-07 16:36:29 +0530

    Update fetchFromAPI.js
    changing key of rapid api

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': "f9d1ccb96amshfe97ad994947ac4p1b3907jsnb9597d71bd81",
+    'X-RapidAPI-Key':'f9d1ccb96amshfe97ad994947ac4p1b3907jsnb9597d71bd81',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
